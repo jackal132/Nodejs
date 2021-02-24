69e0c4cb3e5d69be49dc096e7a8d39a70f8b4a9b
DESKTOP-2A54VHJ\user <jackal132@naver.com>
2021-02-24 14:15:57 +0900

    동기 비동기

---
+++ sync/sync.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+
+/*
+console.log('A');
+var result = fs.readFileSync('sync/sample.txt','utf8');
+console.log(result);
+console.log('C');
+*/
+
+console.log('A');
+fs.readFile('sync/sample.txt','utf8', function(err, result){
+    console.log(result);
+});
+console.log('C');
