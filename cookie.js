bbfe39bfea1ec4759da07c7730e7a4c7f2c88686
DESKTOP-2A54VHJ\user <jackal132@naver.com>
2021-02-25 13:59:28 +0900

    쿠키의 생성

---
+++ cookie.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var cookie = require('cookie');
+http.createServer(function(request, response){
+    var cookies = {};
+    if(request.headers.cookie !== undefined){
+        cookies = cookie.parse(request.headers.cookie);
+    }
+    response.writeHead(200, {
+        'Set-Cookie': ['yummy_cookie=choco','tasty_cookie=strawberry']
+    });
+
+    response.end('cookie!!');
+}).listen(3000);
